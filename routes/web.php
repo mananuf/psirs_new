2aeac7a7228a9682048fe2fc23e34bb233d6ebb5
jaylinkz <johnsonjefferson64@gmail.com>
2023-10-17 04:27:29 +0100

    bugs fixed

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,18 @@
 
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\manageImagesController;
+use  App\Http\Controllers\categoryController;
+use App\Http\Controllers\blogController;
 
 Route::get('/', MainController::class)->name('home');
 
 // Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::resource('/images', [App\Http\Controllers\manageImagesController::class])->name('images');
-Route::resource('/category', [App\Http\Controllers\categoryController::class])->name('category');
-Route::resource('/blog', [App\Http\Controllers\blogController::class])->name('blog');
+Route::resource('/images',manageImagesController::class);
+Route::resource('/category',categoryController::class);
+Route::resource('/blog',blogController::class);
 Route::get('/dash',function(){
     return view('admin.dashboard');
 
